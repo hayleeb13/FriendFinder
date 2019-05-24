3cebbd1a7425e604566fe10427f21c8e5dbd62b6
hayleeb13 <hayleeb13@gmail.com>
2019-05-24 01:28:00 -0700

    Updated friends and survey file

---
+++ app/data/friends.js
@@ -0,0 +1,82 @@
+var scoreArray = [
+  {
+    name: "Barry Allen",
+    photo: "https://i.pinimg.com/originals/f4/f0/29/f4f029b0d048a710757cf2fa41c204df.jpg",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "John Smith",
+    photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Diana Prince",
+    photo: "https://upload.wikimedia.org/wikipedia/en/thumb/9/93/Wonder_Woman.jpg/250px-Wonder_Woman.jpg",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Rick Sanchez",
+    photo: "https://vignette.wikia.nocookie.net/rickandmorty/images/a/a6/Rick_Sanchez.png/revision/latest?cb=20160923150728",
+    scores: [
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+    ]
+  },
+  {
+    name: "Scooby Doo",
+    photo: "https://i.pinimg.com/originals/91/5a/ed/915aeddee3f1a6cb2ceb2c48f4bd9974.jpg",
+    scores: [
+      "5",
+      "5",
+      "1",
+      "4",
+      "3",
+      "2",
+      "4",
+      "1",
+      "4",
+      "2",
+    ]
+  }
+];
