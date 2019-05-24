bba75ae42d878d6608827cc9fdd436e9def3a5f4
hayleeb13 <hayleeb13@gmail.com>
2019-05-24 02:49:34 -0700

    Fixed apiRoutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,11 +2,13 @@ var friends = require("../data/friends");
 
 module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
-    res.json(friends);
+    return res.json(friends);
   });
 
   app.post("/api/friends", function(req, res) {
-    friends.push(req.body);
-    res.json(true);
+    var newFriends = req.body;
+    newFriends.friends = newFriends.name.replace(/\s+/g, "").toLowerCase();
+    friends.push(newFriends);
+    res.json(newFriends);
   });
 };
