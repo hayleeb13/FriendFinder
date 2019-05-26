a5414d8b9aa536f029d0acc7699f8aec7f468d90
hayleeb13 <hayleeb13@gmail.com>
2019-05-25 21:33:40 -0700

    Still working on apiRoutes

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 var express = require("express");
-var path = require("path");
 var app = express();
-var bodyParser = require("body-parser");
 var PORT = process.env.PORT || 3306;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(bodyParser.text());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('data'));
+app.use(express.static('public'));
 
-require(path.join(__dirname, "./app/routing/htmlRoutes"))(app);
-require(path.join(__dirname, "./app/routing/apiRoutes"))(app);
+require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
